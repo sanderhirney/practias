59cbea9c3b12566d25941547df19fb4a6263ef5f
sander <sander@InformaticaHSDM>
2024-06-19 10:54:48 -0400

    CargaInicial

---
+++ src/main/java/shca/inventario/Inventario.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package shca.inventario;
+
+/**
+ *
+ * @author sander
+ */
+public class Inventario {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
