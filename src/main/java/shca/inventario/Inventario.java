92a6bb87a2d6fec42ced2d6ccecca7d9479da0f3
sander <sander@InformaticaHSDM>
2024-09-11 12:59:06 -0400

    Actualizaciones al 11/09/2024 parte 2

--- src/main/java/shca/inventario/Inventario.java
+++ src/main/java/shca/inventario/Inventario.java
@@ -3,6 +3,7 @@ package shca.inventario;
 import inventario.Ventana_Principal;
 import java.io.File;
 import java.io.IOException;
+import java.net.ServerSocket;
 import java.net.URL;
 import java.time.LocalDate;
 import java.time.Month;
@@ -16,12 +17,12 @@ import javax.swing.JOptionPane;
 
 public class Inventario {
 private final static Logger Log = Logger.getLogger("Inventario");
-  
+private static ServerSocket socket;
 public static void main(String[] args) {
                try
         { 
             
-            
+            socket=new ServerSocket(3672);
          SimpleFormatter Formato = new SimpleFormatter();
         Handler consolaErrores=new ConsoleHandler();
         Handler archivo=new FileHandler("log.txt");
@@ -31,11 +32,11 @@ public static void main(String[] args) {
         archivo.setFormatter(Formato);
         Log.addHandler(consolaErrores);
         Log.addHandler(archivo);
-        /*Ventana_Principal ventana = new Ventana_Principal();
+        Ventana_Principal ventana = new Ventana_Principal();
         ventana.setLocationRelativeTo(null);
         ventana.setResizable(false);
-        ventana.setVisible(true);*/
-        PuntoDeEntrada.getInicio();
+        ventana.setVisible(true);
+      
         
         Log.log(Level.ALL, "Bitacora: ");
         Log.log(Level.INFO, "Ingreso Satisfactorio");
@@ -45,7 +46,7 @@ public static void main(String[] args) {
                     
         }catch(IOException | SecurityException e)
         {
-           JOptionPane.showMessageDialog(null, "Se ha producido un error al cargar los archvios de arranque" + "\n Verifique que los archivos de LOG y del sistema esten ubicados apropiadamente" + e, "Error", JOptionPane.ERROR_MESSAGE);
+           JOptionPane.showMessageDialog(null, "Ya esta el sistema abierto"+"\nSe ha producido un error al cargar los archvios de arranque" + "\n Verifique que los archivos de LOG y del sistema esten ubicados apropiadamente"+ e, "Error", JOptionPane.ERROR_MESSAGE);
             
         }
 
